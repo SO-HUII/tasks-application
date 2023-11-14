13f026426b98ab449f16bd3a2cc18f4c8defe275
SO HUI <haejuuun925@gmail.com>
2023-11-14 21:58:41 +0900

    feat: NewTask, TaskForm 컴포넌트 구현 및 스타일링

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+import NewTask from './components/New Tasks/NewTask';
+
+function App() {
+  return (
+    <Fragment>
+      <NewTask onAddTask={} />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/New Tasks/NewTask.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+
+const NewTask = (props) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const enterTaskHandler = async (taskText) => {
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch(
+                'https://tasks-ae2cf-default-rtdb.firebaseio.com/',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({ text: taskText }),
+                    headers: {
+                        'Context-Type': 'application/json',
+                    },
+                }
+            );
+            
+            if(!response.ok) {
+                throw new Error('Request failed!');
+            }
+
+            const data = await response.json();
+
+            const generatedId = data.name;
+            const createdTask = { id: generatedId, text: taskText };
+
+            props.onAddTask(createdTask);
+        } catch(err) {
+            setError(err.message || 'Something went wrong');
+        }
+        setIsLoading(false);
+    };
+
+    return (
+        <Section>
+            <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+            {error && <p>{error}</p>}
+        </Section>
+    );
+};
+
+export default NewTask;
